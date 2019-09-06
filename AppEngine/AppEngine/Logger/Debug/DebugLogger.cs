a515f95081af4643c0693e11cc15aee2bdf2c10d
itskhawarizmi <itskhawarizmi@gmail.com>
2019-09-06 20:51:58 +0700

    create debug logger

---
+++ AppEngine/AppEngine/Logger/Debug/DebugLogger.cs
@@ -0,0 +1,10 @@
+﻿namespace AppEngine
+{
+    public class DebugLogger : ILogger
+    {
+        public void Log(string message, LogLevel Level)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
