ddeb1ed16d194d0e5833939b0afb9dad6eb7fb73
itskhawarizmi <itskhawarizmi@gmail.com>
2019-09-06 20:50:11 +0700

    add base log factory for AppEngine

--- AppEngine/AppEngine/Logger/BaseLogFactory.cs
+++ AppEngine/AppEngine/Logger/BaseLogFactory.cs
@@ -1,11 +1,100 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace AppEngine
 {
-    public class BaseLogFactory
+    /// <summary>
+    /// The standard log factory for AppEngine
+    /// Logs details to the Debug by default
+    /// </summary>
+    public class BaseLogFactory : ILogFactory
     {
 
+        #region Properties
+
+        /// <summary>
+        /// The list of logger in this factory.
+        /// </summary>
+        protected List<ILogger> loggers = new List<ILogger>();
+
+        /// <summary>
+        /// A lock for the logger list to keep it thread safe.
+        /// </summary>
+        protected object loggersLock = new object();
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="loggers">The loggers to add to the factory, on top of the stock loggers already included</param>
+        public BaseLogFactory(ILogger[] loggers = null)
+        {
+            AddLogger(new DebugLogger());
+
+            if (loggers != null)
+                foreach (var logger in loggers)
+                    AddLogger(logger);
+        }
+
+        #endregion
+
+        public event Action<(string message, LogLevel level)> NewLog;
+
+        /// <summary>
+        /// Adds the specific logger to this factory.
+        /// </summary>
+        /// <param name="logger">The logger</param>
+        public void AddLogger(ILogger logger)
+        {
+            lock (loggersLock)
+            {
+                if (!loggers.Contains(logger))
+                    loggers.Add(logger);
+            }
+        }
+
+        /// <summary>
+        /// Removes the specified logger from this factory.
+        /// </summary>
+        /// <param name="logger">The logger</param>
+        public void RemoveLogger(ILogger logger)
+        {
+            lock (loggersLock)
+            {
+                if (loggers.Contains(logger))
+                    loggers.Remove(logger);
+
+            }
+        }
+
+        /// <summary>
+        /// Logs the specific message to all logger in this factory.
+        /// </summary>
+        /// <param name="message">The message to log</param>
+        /// <param name="level">The level of the message to logged</param>
+        /// <param name="memberName">The method/function this message was logged from</param>
+        /// <param name="filePath">The file of location that this message was logged from</param>
+        /// <param name="number">The line of code in this message was logged from</param>
+        public void Log(string message, LogLevel level = LogLevel.Debug, 
+            [CallerMemberName] string memberName = "", 
+            [CallerFilePath] string filePath = "", 
+            [CallerLineNumber] int LineNumber = 0)
+        {
+            message = $"Message: [{message}] - " +
+                      $"Location: [{Path.GetFullPath(filePath)}] - " +
+                      $"File Name: [{Path.GetFileName(memberName)}] - " +
+                      $"Line: [{LineNumber}]";
+
+            loggers.ForEach(loggers => loggers.Log(message, level));
+
+            NewLog.Invoke((message, level));
+
+        }
     }
 }
