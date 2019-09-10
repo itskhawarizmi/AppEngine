8a7160b639cea15de300037f7ad0f62ea0299e8a
itskhawarizmi <itskhawarizmi@gmail.com>
2019-09-10 11:08:09 +0700

    added debug logger

---
+++ AppEngine/AppEngine/Logger/LogModel/Debug/DebugLogger.cs
@@ -0,0 +1,59 @@
+﻿using System.Diagnostics;
+
+namespace AppEngine
+{
+    /// <summary>
+    /// Logs the messages to the Debug log.
+    /// </summary>
+    public class DebugLogger : ILogger
+    {
+        /// <summary>
+        /// Handles the logged message.
+        /// </summary>
+        /// <param name="message">The message to log</param>
+        /// <param name="level">The level of the log message</param>
+        public void Log(string message, LogLevel Level)
+        {
+
+            var category = default(string);
+
+            switch (Level)
+            {
+                case LogLevel.Trace:
+                    category = "[TRACE]";
+                    break;
+
+                case LogLevel.Debug:
+                    category = "[DEBUG]";
+                    break;
+
+                case LogLevel.Information:
+                    category = "[INFORMATION]";
+                    break;
+
+                case LogLevel.Warning:
+                    category = "[WARNING]";
+                    break;
+
+                case LogLevel.Error:
+                    category = "[!ERROR!]";
+                    break;
+
+                case LogLevel.Critical:
+                    category = "[!CRITICAL!]";
+                    break;
+
+                case LogLevel.None:
+                default:
+                    category = "[--------]";
+                    break;
+
+
+            }
+
+
+
+            Debug.WriteLine(message, $"Alert: {category} ");
+        }
+    }
+}
